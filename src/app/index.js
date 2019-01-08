510d961c4f71188ac372599b7e6cf229f8bc6baf
Nick Quebbeman <n.quebb@gmail.com>
2019-01-08 10:26:26 -0800

    cleanup the <App /> rendering

--- src/app/index.js
+++ src/app/index.js
@@ -8,38 +8,24 @@ import createStore from '../shared/store'
 import Route from './components/Route'
 
 if (typeof window !== 'undefined') {
+  const root = document.getElementById('app')
   const { initialProps, initialState, path } = window.__SSR_DATA__
+  const store = createStore(initialState)
 
-  const Router = ({ url, initialProps }) => {
-    return (
-      <PreactRouter url={url}>
+  const App = () => (
+    <Provider store={store}>
+      <PreactRouter>
         {routes.map(route => {
           // Only the current route needs `initialProps`
           const props = path === route.path ? { ...route, ...initialProps } : route
           return <Route key={route.path} {...props} />
         })}
       </PreactRouter>
-    )
-  }
-
-  const App = ({ store, ...props }) => {
-    return (
-      <Provider store={store}>
-        <Router {...props} />
-      </Provider>
-    )
-  }
-
-  // Remove server rendered js from the document
-  // No clue if this does anything, really..
-  delete window.__SSR_DATA__
-  const el = document.getElementById('__SSR_DATA__')
-  el.parentNode.removeChild(el)
+    </Provider>
+  )
 
   // Render the app.
-  const root = document.getElementById('app')
-  const store = createStore(initialState)
-  render(<App store={store} initialProps={initialProps} />, root, root.lastChild)
+  render(<App />, root, root.lastChild)
 
   // Register service worker
   if ('serviceWorker' in navigator && window.location.protocol === 'https') {
@@ -49,4 +35,10 @@ if (typeof window !== 'undefined') {
         .catch(e => { console.log('SW registration failed: ', e) })
     })
   }
+
+  // Remove server rendered js from the document
+  // No clue if this does anything, really..
+  delete window.__SSR_DATA__
+  const el = document.getElementById('__SSR_DATA__')
+  el.parentNode.removeChild(el)
 }
