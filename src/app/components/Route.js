d51d9527db6c90446786a199ec7f72cc752b190a
Nick Quebbeman <n.quebb@gmail.com>
2019-11-22 14:04:40 -0800

    rejigger, and hopefully simplify, <Route />

--- src/app/components/Route.js
+++ src/app/components/Route.js
@@ -1,19 +1,41 @@
 import { h } from "preact";
+import { useState, useEffect } from "preact/hooks";
 import { useRoute } from "wouter-preact";
 
-import useInitialProps from "../lib/useInitialProps";
-
-export default ({ path, match, store, ...props }) => {
+/**
+ * Handle
+ */
+export default ({ path, match, store, component, getComponent }) => {
 	const useRouteMatch = useRoute(path);
+	const [[Component, initialProps], setRoute] = useState([component]);
 
 	// `match` is an array from Switch and NestedRouter
 	const [matches, params] = match || useRouteMatch;
-
 	if (!matches) return null;
 
-	const [Component, initialProps] = useInitialProps({ ...props, params, store });
+	useEffect(async () => {
+		let c = component,
+			iP = {};
 
-	return Component 
-		? <Component {...({ path, params, ...initialProps })} /> 
-		: null;
+		if (!c) {
+			const m = await getComponent();
+			c = m.default || m;
+		}
+
+		if (c.getInitialProps) iP = await c.getInitialProps({ params, store });
+
+		setRoute([c, iP]);
+		return () => null;
+	}, []);
+
+	// If no component is present return null.
+	if (!Component) return null;
+
+	// If the component doesn't need `initialProps` render the component.
+	if (!Component.getInitialProps)
+		return <Component {...{ path, params }} />;
+
+	// Wait for `initialProps` in order to render the component
+	if (Component.getInitialProps && initialProps) 
+		return <Component {...{ path, params, ...initialProps }} />
 };
